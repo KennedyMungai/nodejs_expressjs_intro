415014cfd2c1c73096ccf6acaa2f5e15a33caa0c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-17 12:15:32 +0300

    Did that for the new route

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
-app.get('/new-page.html', (req, res) => {
+app.get('/new-page(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
 })
 
