9f4d47d595f33ae09892c4c30c37ce708f8bb036
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-17 12:08:26 +0300

    Used sendFile to get the server page being requested

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express()
 const PORT = process.env.PORT || 3500
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname })
+    // res.sendFile('./views/index.html', { root: __dirname })
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
