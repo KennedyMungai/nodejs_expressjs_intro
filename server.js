0b0033ef8152d10e520a7583437737123f49388b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-17 12:02:36 +0300

    Called the get function

--- server.js
+++ server.js
@@ -4,5 +4,8 @@ const app = express()
 
 const PORT = process.env.PORT || 3500
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
